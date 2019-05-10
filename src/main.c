505c07682d10a88de48938f8537fc790d908ff31
LucasRodolfo <lucas.rodolfo.moura@gmail.com>
2019-05-10 13:46:38 -0300

    primeira versao com race condition

--- src/main.c
+++ src/main.c
@@ -6,12 +6,76 @@
  */
 
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int ehPrimo(int numero){
+      int flag = 0;
+
+      for(int i = 2; i <= numero/2; ++i){
+          if(numero%i == 0){
+              flag = 1;
+              break;
+          }
+      }
+
+      if (numero == 1 || numero == 0){
+        return 0;
+      }
+      else{
+          if (flag == 0){
+            return 1;
+          }else{
+            return 0;
+          }
+      }
+}
 
 int main() {
 
-  int x, y;
+  char temp;
+  int vetor[200];
+  int i=0, tamanho=0, j=0;
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+  pid_t filhos[4];
+
+  int protection = PROT_READ | PROT_WRITE;
+  int visibility = MAP_SHARED | MAP_ANON;
+
+  int *NPrimos; /* Numero de elementos que ja foram processados */
+  NPrimos = (int*) mmap(NULL, sizeof(int), protection, visibility, 0, 0);
+  if (NPrimos==(int*)-1) printf("Erro de alocacao!\n");
+  (*NPrimos) = 0;
+
+  do {
+    scanf("%d%c", &vetor[i], &temp);
+    i++;
+  } while(temp != '\n');
+  tamanho =i;
+
+  for (int i = 0; i < tamanho; i++) {
+    if (ehPrimo(vetor[i])) {
+      printf("%d é primo\n", vetor[i] );
+    }
+  }
+  for (i = 0; i < tamanho; i+=4) {
+    /* Create the processes */
+    for (j=0; j<4; j++) {
+        filhos[j] = fork();
+        if (filhos[j] == 0) {
+          (*NPrimos) += ehPrimo(vetor[i+j]);
+          exit(0);
+        }
+      for (int j=0; j<4; j++) {
+        waitpid(filhos[j], NULL, 0);
+      }
+    }
+  }
+  printf("%d\n", *NPrimos);
+
   return 0;
 }
